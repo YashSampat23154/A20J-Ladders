33b8a2495d11b83678c11a4e64e756c5c09b1a9f
Yash Sampat <yashsampat23154@gmail.com>
2024-07-24 07:51:28 +0530

    A. Team - Codeforces

---
+++ Codeforces rating < 1300/28. A. Team.cpp
@@ -0,0 +1,44 @@
+// #pragma GCC optimize("Ofast", "unroll-loops")
+// #pragma GCC target("avx2", "tune=native")
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define what(x) cerr << #x << " is " << x << endl;
+
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+
+typedef long long ll;
+
+using namespace std;
+ 
+int main()
+{ 
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    fastio;
+    
+    int t = 1;
+    cin>>t;
+
+    int count = 0; 
+
+    while(t--){
+        int a, b, c; 
+        cin>>a>>b>>c; 
+
+        if(a+b+c >= 2)
+            count++; 
+    }
+
+    cout << count << endl;
+    
+    return 0;
+}
+
