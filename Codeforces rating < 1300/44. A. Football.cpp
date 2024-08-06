0b4e3afcdecc8c1f813b1d8ae3dab8586dcda004
Yash Sampat <yashsampat23154@gmail.com>
2024-08-06 08:54:08 +0530

    A. Football - Codeforces

---
+++ Codeforces rating < 1300/44. A. Football.cpp
@@ -0,0 +1,50 @@
+// #pragma GCC optimize("Ofast", "unroll-loops")
+// #pragma GCC target("avx2", "tune=native")
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define what(x) cerr << #x << " is " << x << endl;
+
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+
+typedef long long ll;
+
+using namespace std;
+ 
+int main()
+{ 
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    fastio;
+    
+    int n; 
+    cin>>n; 
+
+    unordered_map<string, int> mp; 
+    for(int i = 0; i<n; i++){
+        string s; 
+        cin>>s; 
+        mp[s]++; 
+    }
+
+    string ans = "";
+    int maxi = 0;
+    for(auto it: mp){
+        if(it.second > maxi){
+            ans = it.first; 
+            maxi = it.second; 
+        }
+    }
+
+    cout << ans << endl;
+    
+    return 0;
+}
+
