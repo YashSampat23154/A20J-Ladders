eebd089652371ecbf6c19dfda6b66a63a4fad790
Yash Sampat <yashsampat23154@gmail.com>
2024-08-12 08:14:57 +0530

    A. System of Equation - Codeforces

---
+++ Codeforces rating < 1300/47. A. System of Equations.cpp
@@ -0,0 +1,43 @@
+// #pragma GCC optimize("Ofast", "unroll-loops")
+// #pragma GCC target("avx2", "tune=native")
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define what(x) cerr << #x << " is " << x << endl;
+
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+
+typedef long long ll;
+
+using namespace std;
+ 
+int main()
+{ 
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    fastio;
+    
+    int n, m; 
+    cin>>n>>m; 
+
+    int count = 0; 
+
+    for(int i = 0; i*i <= n; i++){
+        for(int j = 0; j*j <= m-i; j++){
+            if((i*i + j) == n && (i + j*j) == m)
+                count++; 
+        }
+    }
+
+    cout << count << endl;
+    
+    return 0;
+}
+
