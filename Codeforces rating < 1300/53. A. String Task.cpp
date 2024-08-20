9851123bca32b0d0f0849711367618674857b00e
Yash Sampat <yashsampat23154@gmail.com>
2024-08-20 08:13:58 +0530

    A. String Task - Codeforces

---
+++ Codeforces rating < 1300/53. A. String Task.cpp
@@ -0,0 +1,48 @@
+// #pragma GCC optimize("Ofast", "unroll-loops")
+// #pragma GCC target("avx2", "tune=native")
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define what(x) cerr << #x << " is " << x << endl;
+
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+
+typedef long long ll;
+
+using namespace std;
+ 
+int main()
+{ 
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    fastio;
+    
+    string s; 
+    cin>>s; 
+
+    string newS = ""; 
+
+    for(int i = 0; i<s.length(); i++){
+        if(s[i] >= 65 && s[i] <=90)
+            s[i]+=32; 
+        
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y')
+            continue; 
+        else{
+            newS+=".";
+            newS+=s[i];
+        }
+    }
+
+    cout << newS << endl;
+    
+    return 0;
+}
+
