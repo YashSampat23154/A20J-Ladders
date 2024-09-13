bc71a9dfab82b44ae6b8a669a5ee7c02ee77a127
Yash Sampat <yashsampat23154@gmail.com>
2024-09-13 08:54:50 +0530

    B. Hungry Sequence - Codeforces

---
+++ Codeforces rating < 1300/71. B. Hungry Sequence.cpp
@@ -0,0 +1,36 @@
+// #pragma GCC optimize("Ofast", "unroll-loops")
+// #pragma GCC target("avx2", "tune=native")
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define what(x) cerr << #x << " is " << x << endl;
+
+#define vi vector<int>
+#define vll vector<long long>
+#define endl "\n"
+
+typedef long long ll;
+
+using namespace std;
+ 
+int main()
+{ 
+    
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    fastio;
+    
+    int n;
+    cin>>n; 
+
+    for(int i = 10000000-n; n--; i++)
+        cout << i << " "; 
+    cout << endl;
+    
+    return 0;
+}
+
